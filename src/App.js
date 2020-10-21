23e1ed738bcbb7a57ec0ebacb6c18576f24ed3d1
Iris Yu <iris.yu0716@hmail.com>
2020-10-21 23:12:14 +0800

    build evironment and test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MyNavbar from "./components/commons/MyNavbar";
+import MainContent from "./components/pages/MainContent";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <>
+          <MyNavbar />
+          <MainContent />
+        </>
+      </Router>
     </div>
   );
 }
